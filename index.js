cfab1b170e1e181705e0cc591ab77701e08275f4
Richard van der Dys <rhvanderdys@containerstore.com>
2015-01-24 20:05:32 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/* jslint node:true */
+
+var request = require('request');
+var cheerio = require('cheerio');
+var _ = require('underscore');
+var program = require('commander');
+
+var nameWithScopePattern = /(\w+)\((.*)\)/;
+
+var log = function(o) {
+	if (program.verbose) {
+		console.log(o);
+	}
+};
+
+function select($, selector, filter) {
+	if (_.isObject(selector)) {
+		return selectObject($, selector);
+	} else if (_.isArray(filter)) {
+		return selectList($, selector, filter[0]);
+	} else if (_.isString(selector)) {
+		return selectData($, selector);
+	} else {
+		throw new Error('Selector not allowed: ' + selector);
+	}
+}
+
+function selectObject($, dataMap) {
+	log('parsing data map');
+	var data = {};
+	_.each(dataMap, function(filter, name) {
+		var temp = name.match(nameWithScopePattern);
+		if (temp) {
+			name = temp[1];
+			var scope = temp[2];
+			log('selecting name with scope: ' + name + '/' + scope);
+			data[name] = select($, scope, filter);
+		} else {
+			log('selecting name: ' + filter);
+			data[name] = select($, filter);
+		}
+	});
+	return data;
+}
+
+function selectList($, selector, dataMap) {
+	log('parsing list');
+	var results = [];
+	$(selector).each(function() {
+		results.push(select($(this), dataMap));
+	});
+	return results;
+}
+
+function findSelector(el, selector) {
+	return el.find ? el.find(selector) : el(selector);
+}
+
+function selectAttr(el, selector) {
+	var temp = selector.split('@');
+	selector = temp[0].trim();
+	var attrName = temp[1].trim();
+	return selector.length > 0 ? findSelector(el, selector).attr(attrName) : el.attr(attrName);
+}
+
+function selectData(el, selector) {
+	log('selecting data');
+	var value;
+	if (selector === '.') {
+		value = el.text();
+	} else if (selector.indexOf('@') !== -1) {
+		value = selectAttr(el, selector);
+	} else {
+		value = findSelector(el, selector).text();
+	}
+	return value;
+}
+
+function parse(parselet, url, cb) {
+	request(url, function(err, res, html) {
+		if (!err && res.statusCode === 200) {
+			var $ = cheerio.load(html);
+			cb(null, select($, parselet));
+		} else {
+			cb(err || new Error('URL returned a status of ' + res.statusCode));
+		}
+	});
+}
+
+if (require.main === module) {
+	program
+		.version('0.0.1')
+		.option('-v, --verbose', 'Verbose mode')
+		.option('-u, --url <path>', 'URL to parse')
+		.option('-p, --parselet <path>', 'Path to parselet')
+		.parse(process.argv);
+
+	parse(require(program.parselet), program.url, function(err, data) {
+		if (err) {
+			console.log(err);
+		}
+		console.log(JSON.stringify(data, null, '\t'));
+	});
+} else {
+	module.exports = parse;
+}
---
+++ test/spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/* jshint node:true */
+/* globals describe, it */
+
+require('should');
+var parsz = require('../index');
+var nock = require('nock');
+var fs = require('fs');
+
+nock('http://www.test.com')
+	.get('/')
+	.times(3)
+	.reply(200, fs.readFileSync(__dirname + '/index.html', 'UTF-8'));
+
+describe('Parsley', function() {
+	it('should parse plain selectors', function(done) {
+		parsz({
+			title: 'h1'
+		}, 'http://www.test.com', function(err, data) {
+			data.title.should.equal('Hello World!');
+			done();
+		});
+	});
+	it('should parse list of elements', function(done) {
+		parsz({
+			'links(ul a)': [{
+				name: '.',
+				href: '@href'
+			}]
+		}, 'http://www.test.com', function(err, data) {
+			data.links.length.should.equal(3);
+			data.links[0].name.should.equal('A');
+			data.links[0].href.should.equal('/a');
+			done();
+		});
+	});
+	it('should parse attributes', function(done) {
+		parsz({
+			published: '[itemprop=date-published] @content'
+		}, 'http://www.test.com', function(err, data) {
+			data.published.should.equal('01/01/2015');
+			done();
+		});
+	});
+});
